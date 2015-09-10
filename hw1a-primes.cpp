a3a61d4478208d814bea6b85aadf1c0bc445e345
Dylan Praul <dpraul@ionfish.org>
2015-09-09 21:10:26 -0400

    It works, just not for large numbers

--- hw1a-primes.cpp
+++ hw1a-primes.cpp
@@ -1,50 +1,69 @@
 #include<iostream>
-#include<math.h>
+#include<math.h>  // for sqrt
 #include<stdlib.h>  // for strtol
+#include<vector>
 
 using namespace std;
 
-const long long TABLE_LEN = 100000000000000;
+int f(int a, int b, int c) {
+	if (a <= 0)
+		return a + b * c;
+	return f(a - 1, b + 1, c + 2);
+}
 
 int main(int argc, char *argv[]) {
 	unsigned long long min, max;
-	/*
-	if (argc == 3) {
+	
+	if (argc == 3) {  // If available, get numbers from arg string -- only works for ints
 		min = strtol(argv[1], NULL, 10);  // a
 		max = strtol(argv[2], NULL, 10);  // b
 	}
-	else {
-		cout << "Enter a (a < 10^14): ";
+	else {  // Else, have user enter in.
+		cout << "requirements: a < b < 10^14" << endl;
+		cout << "Enter a (min): ";
 		cin >> min;
-		cout << "Enter b (a < b < 10^14): ";
+		cout << "Enter b (max): ";
 		cin >> max;
 	}
-	*/
-	min = 2000000000000;
-	max = 2000000100000;
-	if (min > max) {
+	cout << "a = " << min << ", b = " << max << endl;
+
+	// Assure the entered values fit the spec.
+	if (min >= max) {
 		cout << "b must be larger than a" << endl;
 		return 0;
 	}
 
-	bool* not_prime = new bool[TABLE_LEN];
-
+	vector<bool> composite(max + 1, false);
 	unsigned long long i, j;
+	// Manually set 0 and 1 to composite
+	composite[0] = true;
+	composite[1] = true;
 
-	for (i = 2; i <= max; i ++) {
-		if (!not_prime[i - 1]) {
-			for (j = i * i; j <= max; j += 2 * i) {
-				not_prime[j - 1] = true;
+	// Set all even numbers to composite
+	//!! In testing, this turned out to be slower than checking whether
+	//!! the number was even during each loop, so it was commented out
+	//for (i = 4; i <= max; i += 2) {
+	//	composite[i] = true;
+	//}
+	// Use Erotosthenes sieve to mark remaining numbers.
+	for (i = 3; i <= (int)sqrt(max); i += 2) {
+		if (!composite[i] && i % 2 != 0) {
+			for (j = i * i; j <= max; j += (i * 2)) {
+				composite[j] = true;
 			}
 		}
 	}
 
+	// Count primes from min to max
 	unsigned long long count = 0;
-	for (i = min; i <= max; i += 2) {
-		if (!not_prime[i - 1]) {
+	// 2 is the only prime even though it's even, so include if needed
+	if (min <= 2) {
+		count++;
+	}
+	for (i = min; i <= max; i++) {
+		if (!composite[i] && i % 2 != 0) {
 			count++;
 		}
 	}
-
-	cout << count;
+	cout << "Primes: " << count;
 }

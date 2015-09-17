f70b820487c3255d3fa4b116ed556aafb70e9cfe
Dylan Praul <dpraul@ionfish.org>
2015-09-16 20:55:36 -0400

    Finished quick sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<fstream>
-#include<stdlib.h> // rand
+#include<stdlib.h>
+#include<time.h> // time
 
 using namespace std;
 
-void print_arr(int x[], int length) {
-	for (int i = 0; i < length; ++i) {
+
+void print_arr(int x[], int start, int end) {
+	for (int i = start; i < end; ++i) {
 		cout << x[i] << ' ';
 	}
 	cout << endl;
@@ -14,11 +16,12 @@ void print_arr(int x[], int length) {
 
 void quicksort(int x[], int left, int right) {
 	int i = left,
-		j = right,
+		j = right - 1,
 		tmp,
-		pivot = x[rand() % (right + 1)];
-	cout << pivot << ' ' << left << ' ' << right << endl;
-	print_arr(x, right + 1);
+		pivot = x[(rand() % (right - left)) + left];
+	if (left >= right) {
+		return;
+	}
 	while (i <= j) {
 		while (x[i] < pivot) {
 			++i;
@@ -37,37 +40,42 @@ void quicksort(int x[], int left, int right) {
 	}
 	
 	if (left < j) {
-		quicksort(x, left, j);
+		quicksort(x, left, j + 1);
 	}
-	if (i > right) {
-		quicksort(x, i, right);
+	if (i < right) {
+		quicksort(x, j + 1, right);
 	}
 }
 
+
 int main(int argc, char *argv[]) {
-	char *filename;
-	if (argc == 1) {
+	string filename;
+	if (argc == 2) {  // Get filename from args, if available.
 		filename = argv[1];
 	}
-	else {
+	else {  // Otherwise, prompt for filename.
 		cout << "Enter filename: ";
 		cin >> filename;
 	}
-	ifstream file;
-	string line;
+	srand(time(0)); // seed random number generator
 	
-	int length;
-	string arr;
+	int * x;
+	int length, i = 0;
+	
+	ifstream file;
 	file.open(filename);
+	
 	if (file.is_open()) {
-		fstream(getline(file, line), length);
-		getline(file, arr);
+		file >> length;  // First number from file is length
+		x = new int[length];
+		while (!file.eof()) {  // All following numbers go into array.
+			file >> x[i];
+			i++;
+		}
 	}
-	cout << length;
-	cout << arr;
-	//int x[] {9, 2, 5, 4, 11, 3, 1, 3, 5, 6, 7, 2, 8};
-	//cout << sizeof(x) << endl;
-	//print_arr(x, sizeof(x));
-	//quicksort(x, 0, sizeof(x) - 1);
-	//print_arr(x);
+	cout << "Length: " << length << endl << "Array:  ";
+	print_arr(x, 0, length);
+	quicksort(x, 0, length);
+	cout << "Sorted: ";
+	print_arr(x, 0, length);
 }
